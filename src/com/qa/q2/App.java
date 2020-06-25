5ec199ae1d7207ba7d7dbb1f027ee59aa1dc4be7
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-06-25 15:07:38 +0100

    completed q2

--- src/com/qa/q2/App.java
+++ src/com/qa/q2/App.java
@@ -2,6 +2,12 @@ package com.qa.q2;
 
 public class App {
 	public static void main(String args[]) {
-		
+		for (int a = 100; a <= 200; a++) {
+			if (a % 2 == 0) {
+				System.out.println("-");
+			} else {
+				System.out.println("*");
+			}
+		}
 	}
 }
